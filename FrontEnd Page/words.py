f4f2031a86670bba53a3bd261d43c2f76af099ba
anothergeorgecoldham <40017771+anothergeorgecoldham@users.noreply.github.com>
2019-08-10 22:38:11 +0800

    added start of frontend

--- FrontEnd Page/words.py
+++ FrontEnd Page/words.py
@@ -1,6 +1,12 @@
+import re
+import string
 
 with open('adverbs.txt') as a:
     adverbs = a.read()
 a.closed
 
-print(adverbs)
+for line in adverbs:
+    words = re.sub('['+string.punctuation+']', '', line).split()
+
+print(words)
+
